29e733790120c257841742aabd3fc9d9fc99f23a
Nikita-Rave <nikitarave01@gmail.com>
2025-04-19 08:22:28 +0530

    Sample_code committed

---
+++ src/main/java/Terasoft_Technologies/Maven_GitHub_Practice/App.java
@@ -0,0 +1,13 @@
+package Terasoft_Technologies.Maven_GitHub_Practice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/git_practice/Sample_code.java
@@ -0,0 +1,12 @@
+package git_practice;
+
+public class Sample_code {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello");
+		
+
+	}
+
+}
