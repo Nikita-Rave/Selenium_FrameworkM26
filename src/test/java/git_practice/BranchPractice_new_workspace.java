94c10fca3edc9b19c55e3efc9c867e54db2a4525
Nikita-Rave <nikitarave01@gmail.com>
2025-04-22 10:11:59 +0530

    Branch_Practice_TC001

---
+++ src/test/java/git_practice/BranchPractice_new_workspace.java
@@ -0,0 +1,15 @@
+package git_practice;
+
+public class BranchPractice_new_workspace {
+
+	public static void main(String[] args) {
+		System.out.println("1.Pull recent FW");
+		System.out.println("2.Automate Test case");
+		System.out.println("3.Create a local branch");
+		System.out.println("4.Push the code in branch");
+		System.out.println("5.Intimate about Pull request");
+		System.out.println("6.Reviewer");
+
+	}
+
+}
